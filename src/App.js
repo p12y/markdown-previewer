594f6e3c3938524694e90aea7f9abdd67786fb9c
Peter Tyldesley <ptyldesley@schawk.com>
2017-11-20 14:55:52 +0000

    Add markdown conversion & CSS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import marked from 'marked';
+import rawText from './rawText';
+
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {rawText, markdown: ''};
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  createMarkup() {
+    return {
+      __html: marked(this.state.rawText)
+    };
+  }
+
+  handleInput(event) {
+    this.setState({rawText: event.target.value});
+  }
+
   render() {
     return (
       <div className="App">
+        <h1 className="page-title"><span># Markdown Previewer</span></h1>
+        <div className="flex-container">
+          <div className="full-page left">
+            <textarea onChange={this.handleInput} value={this.state.rawText}>
+            </textarea>
+          </div>
+          <div dangerouslySetInnerHTML={this.createMarkup()} className="full-page right">
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/rawText.js
@@ -0,0 +1,64 @@
+const rawText = `
+# What is Markdown?
+
+Markdown is a way to style text on the web. You can control the formatting of a document by adding a few non-alphabetic characters, like \`#\` or \`*\`.
+
+----
+
+# Usage
+
+## Headers
+
+### This is an h3 header
+
+----
+
+## Emphasis
+
+*This text is italic*
+
+**This text is bold**
+
+----
+
+## Lists
+
+### Unordered
+
+* Item 1
+* Item 2
+  * Item 2a
+  * Item 2b
+
+### Ordered
+
+1. Item 1
+1. Item 2
+1. Item 3
+   1. Item 3a
+   1. Item 3b
+
+----
+
+## Blockquotes
+
+As Kanye West said:
+
+> We're living the future so
+> the present is our past.
+
+----
+
+## Inline Code
+
+I think you should use an
+\`<addr>\` element here instead.
+
+----
+
+## Links
+
+The examples above have been taken from [GitHub Guides](https://guides.github.com/features/mastering-markdown/)
+`;
+
+export default rawText;
